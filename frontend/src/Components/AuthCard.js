6d94c4cfff4fb92958d22d85c78dc96003950a39
joshua chivile <joshuachivile@gmail.com>
2024-01-24 13:27:12 +0300

    Made excerpt dynamic based on route

--- frontend/src/Components/AuthCard.js
+++ frontend/src/Components/AuthCard.js
@@ -1,14 +1,25 @@
+import { useLocation } from "react-router";
+import { Link } from "react-router-dom"; 
 import Logo from "../Assets/Logo.svg";
+
 export default function AuthCard({ children }) {
+  const location = useLocation();
+
   return (
     <div className="card-container">
       <div className="sign-in-card" style={{ width: "35em" }}>
         <div className="text-center">
           <img src={Logo} alt="shelter-scape logo" />
           <div className="mt-3 mb-3 excerpt">
-            <h6>
-              Don't have an Account yet? <a href="/">Sign up here</a>
-            </h6>
+            {location.pathname === "/signin" ? (
+              <h6>
+                Don't have an account yet? <Link to="/signup">Sign up here</Link>
+              </h6>
+            ) : (
+              <h6>
+                Already have an account? <Link to="/signin">Sign in here</Link>
+              </h6>
+            )}
           </div>
         </div>
         {children}
