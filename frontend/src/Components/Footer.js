7b84a4bcf851515162ea1c510b8a452ecdb4cdf5
onjorobrian <onjorobrian1@gmail.com>
2024-01-25 20:54:48 +0300

    footertestpassed

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -1,32 +1,13 @@
-import React from "react";
+import React from 'react';
 
-export default function Footer() {
+const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className="footer mt-auto py-3 bg-light">
       <div className="container">
-        <div className="row">
-          <div className="col-sm-6">
-            <h4>Contact Us</h4>
-            <p>Email: info@example.com</p>
-            <p>Phone: +123 456 789</p>
-          </div>
-          <div className="col-sm-6">
-            <h4>Follow Us</h4>
-            {/* Add your social media icons or links here */}
-            <div className="social-icons">
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-facebook"></i>
-              </a>
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-twitter"></i>
-              </a>
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-instagram"></i>
-              </a>
-            </div>
-          </div>
-        </div>
+        <span className="text-muted">© {new Date().getFullYear()} Your Company Name. All Rights Reserved</span>
       </div>
     </footer>
   );
-}
+};
+
+export default Footer;
